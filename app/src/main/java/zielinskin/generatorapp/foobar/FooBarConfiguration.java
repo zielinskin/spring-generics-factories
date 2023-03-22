2c652dc9befdf2ed91781f2b44b5f58953d38984
nzielinski <neil.zielinski@artisanpartners.com>
2023-03-22 16:17:55 -0500

    - cleaning up configuration annotations

--- app/src/main/java/zielinskin/generatorapp/foobar/FooBarConfiguration.java
+++ app/src/main/java/zielinskin/generatorapp/foobar/FooBarConfiguration.java
@@ -3,9 +3,11 @@ package zielinskin.generatorapp.foobar;
 
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("zielinskin.generatorapp.foobar")
 class FooBarConfiguration {
     @Bean
     public GroupedOpenApi foobarDocket() {
